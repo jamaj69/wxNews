71da4a392a4135e2b45803dad98286c18afb5288
Jamaj <jamaj@jamaj.com.br>
2026-03-15 14:21:54 -0300

    Add news reader application files and documentation
    - Update wxAsyncNewsReaderv6.py with latest changes
    - Add QUICK_REFERENCE.md documentation
    - Add shell scripts (wxNews.sh, wxnews.sh)
    - Add desktop entry file wxNewsReader.desktop

---
+++ wxNews.sh
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=HP12C
+Comment=The best viewer for Foo objects available!
+TryExec=/home/jamaj/Downloads/finanx-12c-0.2.8/finanx.sh
+Exec=/home/jamaj/Downloads/finanx-12c-0.2.8/finanx.sh
+Icon=/home/jamaj/Downloads/finanx-12c-0.2.8/icon.png
---
+++ wxnews.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wxAsyncNewsReaderv6.py &
+
